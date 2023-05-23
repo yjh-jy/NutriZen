d10a3c8f575b3955c27d26a0eaa254c45c270a0e
junhan <junhanyoong@gmail.com>
2023-05-23 23:46:32 +0800

    removed unnecessary babel plugin

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,6 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['transform-inline-environment-variables',
-    "module:react-native-dotenv"],
+    plugins: ["module:react-native-dotenv"],
   };
 };
