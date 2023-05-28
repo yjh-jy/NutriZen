fb413de2bb4d815b0302adee576645e8e855a299
junhan <junhanyoong@gmail.com>
2023-05-28 21:45:09 +0800

    called custom hook to toggle onboarded token

--- navigation/index.js
+++ navigation/index.js
@@ -2,10 +2,12 @@ import { useAuthentication } from '../hooks/useAuthentication';
 import UserStack from './userStack';
 import AuthStack from './authStack';
 import OnboardingStack from './onboardingStack';
+import { checkOnboarded } from '../hooks/checkOnboarded';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function RootNavigation() {
   const {user} = useAuthentication();
-  const onboarded = false;
-
+  const {onboarded} = checkOnboarded();
+  // AsyncStorage.clear();
   return user ? (onboarded ? <UserStack /> : <OnboardingStack/> ) : <AuthStack />;
 }
