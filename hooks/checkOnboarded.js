388ec961aebf967561cb7cd41d6278c6296c06eb
junhan <junhanyoong@gmail.com>
2023-05-28 21:44:22 +0800

    added custom hook to toggle onboarding token

---
+++ hooks/checkOnboarded.js
@@ -0,0 +1,22 @@
+import {useEffect, useState} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { auth } from '../firebase';
+
+
+export function checkOnboarded() {
+  const [onboarded, setOnboarded] = useState(false);
+  useEffect(()=> {
+    const unsub = async () => {
+      const ref = auth.currentUser ? await AsyncStorage.getItem(auth.currentUser.uid) : null
+      if (ref !== null) {
+        setOnboarded(true);
+      } else {
+        setOnboarded(false);
+      }
+    };
+    return (() => {unsub()});
+  }, [auth.currentUser]);
+
+  return {onboarded};
+  
+}
