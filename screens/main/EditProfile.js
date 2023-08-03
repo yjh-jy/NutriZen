2004814d7c842aa4a3c4a8465bb0eb9430846c78
junhan <junhanyoong@gmail.com>
2023-08-03 18:11:22 +0800

    fixed profile editing bug

--- screens/main/EditProfile.js
+++ screens/main/EditProfile.js
@@ -1,9 +1,12 @@
-import { StyleSheet, Text, View,KeyboardAvoidingView, ScrollView, TextInput } from 'react-native'
+import { StyleSheet, Text, View,KeyboardAvoidingView, ScrollView, TextInput,TouchableOpacity, Image } from 'react-native'
 import {useState} from 'react'
 import colors from '../../assets/colors/colors'
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import DropdownComponent from '../../components/DropDown';
+import { auth, db } from '../../firebase';
+import { doc, setDoc } from 'firebase/firestore';
+
 
 const EditProfile = ({navigation, route}) => {
     const fitnessDataMap = {
@@ -20,8 +23,10 @@ const EditProfile = ({navigation, route}) => {
     const [height, setHeight] = useState('');
     const [weight, setWeight] = useState('');
     const [fitness, setFitness] = useState(fitnessDataMap[String(route.params.fitnessParam)]);
+    const [profilePic, setProfilePic] = useState(route.params.profilePicParam);
 
-    
+    console.log(fitness);
+
     const genderData = [
       { label: 'male', value: 'male' },
       { label: 'female', value: 'female' },
@@ -34,25 +39,52 @@ const EditProfile = ({navigation, route}) => {
         { label: 'Very active', value: 1.725},
         { label: 'Extra active', value: 1.9 },
     ];
-
     const insets = useSafeAreaInsets();
+    
+    const handleUpdate = async () => {
+      if (name && gender && 13<Number(age) && Number(age)<=80 && 130<=Number(height) && Number(height) <=220 &&  30<=Number(weight) && Number(weight) <=200 && fitness) {
+        const user = auth.currentUser;
+        await setDoc(doc(db, "users", user.uid), {
+        name: name,
+        gender: gender,
+        age: Number(age),
+        height: Number(height),
+        weight: Number(weight),
+        fitness_constant: fitness,
+        profilePic: profilePic
+        });
+        navigation.navigate('Profile')
+        alert('Profile Updated !')
+    } else {
+      alert('Invalid Information keyed. Please try again.')
+    }
+  }
+
     return (
       <View style={{
-          flex:1,
-          paddingTop:insets.top,
-          backgroundColor:colors.backgroundColor
+        flex:1,
+        paddingTop:insets.top,
+        backgroundColor:colors.backgroundColor
       }}
       >
-        <Ionicons name="arrow-back" 
-        color='black'
-        size={30}
-        style={{marginLeft:30}}
-        onPress={()=>{navigation.goBack()}}
-        />
+        <TouchableOpacity onPress={()=>{navigation.goBack()}}>
+          <Ionicons name="arrow-back" 
+          color='black'
+          size={30}
+          style={{marginLeft:30}}
+          />
+        </TouchableOpacity>
+
         <KeyboardAvoidingView>
-        <ScrollView>
-            <View style = {styles.itemsWrapper}>
-            <TextInput
+          <ScrollView>
+              <Image
+              style={styles.profilePic}
+              source={{uri:profilePic}}
+              />
+              <View style = {styles.itemsWrapper}>
+              <Text style = {styles.subtitle}>Name</Text>       
+
+              <TextInput
               style={styles.input}
               autoCapitalize='none'
               autoCorrect = {false}
@@ -62,22 +94,20 @@ const EditProfile = ({navigation, route}) => {
               value = {name}
               placeholder={route.params.nameParam}
               onChangeText={text => setName(text)}
-              >
-              </TextInput>
+              />
 
-              <Text style = {styles.subtitle}>Name</Text>       
-
-            <DropdownComponent 
+              <Text style = {styles.subtitle}>Gender</Text>
+              <DropdownComponent 
               data={genderData} 
               dropdownlabel={gender ? gender : 'Select your gender'} 
               icon = 'venus-mars' 
               value={gender}
               setValue={setGender}
-              margin={20}
+              margin={0}
               height={40}
-              width={200}
+              width={220}
               />  
-              <Text style = {styles.subtitle}>Gender</Text>
+              <Text style = {styles.subtitle}>Age</Text>
 
               <TextInput
               style={styles.input}
@@ -90,10 +120,9 @@ const EditProfile = ({navigation, route}) => {
               value = {age}
               placeholder={String(route.params.ageParam)}
               onChangeText={text => setAge(text)}
-              >
-              </TextInput>
+              />
 
-              <Text style = {styles.subtitle}>Age</Text>
+              <Text style = {styles.subtitle}>Height</Text>
 
               <TextInput
               style={styles.input}
@@ -107,10 +136,9 @@ const EditProfile = ({navigation, route}) => {
               value = {height}
               placeholder={String(route.params.heightParam)}
               onChangeText={text => setHeight(text)}
-              >
-              </TextInput>
-
-              <Text style = {styles.subtitle}>Height</Text>
+              />
+            
+              <Text style = {styles.subtitle}>Weight</Text>
 
               <TextInput
               style={styles.input}
@@ -124,27 +152,28 @@ const EditProfile = ({navigation, route}) => {
               value = {weight}
               placeholder={String(route.params.weightParam)}
               onChangeText={text => setWeight(text)}
-              >
-              </TextInput>
-              
-              <Text style = {styles.subtitle}>Weight</Text>
+              />
+                
+              <Text style = {styles.subtitle}>Fitness Level</Text>
+
               <DropdownComponent 
               data={fitnessData} 
-              dropdownlabel={fitness ? fitness : 'Select your fitness level'} 
+              dropdownlabel={'Select your fitness level'} 
               icon = 'running' 
               value={fitness}
               setValue={setFitness}
-              style={{marginLeft:-10}}
-              margin={20}
+              margin={0}
               height={40}
-              width={200}
+              width={220}
               />
-              <Text style = {styles.subtitle}>Fitness Level</Text>
 
+              <TouchableOpacity style={{marginTop:30}}onPress={handleUpdate}>
+                <Text style={styles.subtitle}>Save</Text>
+              </TouchableOpacity>
 
-            </View>
+              </View>
 
-        </ScrollView>
+          </ScrollView>
         </KeyboardAvoidingView>
 
       </View>
@@ -156,21 +185,30 @@ export default EditProfile
 const styles = StyleSheet.create({
     itemsWrapper: {
         alignItems: 'center',
-        justifyContent: 'center',
-        margin:30   
+      },
+      profilePic: {
+        height:100,
+        width:100,
+        alignSelf:'center',
+        backgroundColor:'white',
+        borderRadius:90,
+        borderWidth:3,
+        borderColor:'white',
+        resizeMode:'cover',
+        marginBottom:10
       },
       input: {
         height:40,
-        width:200,
+        width:220,
         backgroundColor: colors.textFieldColor,
         fontSize: 20,
         fontFamily: "PixeloidSan",
-        padding:10
-        
+        marginBottom:10
+
       },
       subtitle: {
         fontFamily: "PixeloidsSanBold",
         fontSize: 15,
-        padding:20
+        marginBottom:10
       },
 })
