8955f454914af8b5f1e21c1c54666eb445a7432c
junhan <junhanyoong@gmail.com>
2023-08-03 18:11:56 +0800

    added work in progress status

--- screens/main/Notifications.js
+++ screens/main/Notifications.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import colors from '../../assets/colors/colors'
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import FontAwesome from '@expo/vector-icons/FontAwesome5';
 
 const Notifications = () => {
   const insets = useSafeAreaInsets();
@@ -9,14 +10,23 @@ const Notifications = () => {
     <View style={{
         flex:1,
         paddingTop:insets.top,
-        backgroundColor:colors.backgroundColor
-    }}>
+        backgroundColor:colors.backgroundColor,
+        alignItems:'center',
+        justifyContent:'center',
         
-
+    }}>
+      <FontAwesome
+      name='wrench'
+      size={80}
+      />
+      <Text style={{alignSelf:'center', fontFamily:'PixeloidsSanBold', fontSize:20, marginTop:20}}>{'Work in Progress\n'}</Text>
+      <Text style={{alignSelf:'center', fontFamily:'PixeloidsSanBold', fontSize:16}}>{'Swipe left to go back'}</Text>
     </View>
   )
 }
 
 export default Notifications
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+})
